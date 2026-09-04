1de676b8dd677838bbb94fccf6b3b9a0d61122b7
RomanBratchykov <romanbratchykov@gmail.com>
2026-09-04 19:29:26 +0300

    [3] Small postfixes

--- shared/src/commonMain/kotlin/com/example/weatherapichallenge/ui/theme/Theme.kt
+++ shared/src/commonMain/kotlin/com/example/weatherapichallenge/ui/theme/Theme.kt
@@ -1,6 +1,4 @@
 package com.example.compose
-import android.app.Activity
-import android.os.Build
 import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.lightColorScheme
